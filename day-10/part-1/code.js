14194d84d662b7beda91d59cee25cb180be0b9c2
ronilbhatia <ronilbhatia@gmail.com>
2019-12-10 08:42:52 -0800

    Complete Day 10 Part 1

--- day-10/part-1/code.js
+++ day-10/part-1/code.js
@@ -5,14 +5,9 @@ fs.readFile('input.txt', (err, data) => {
   const asteroidCoords = getAsteroidCoords(map);
   let coordStrings = new Set(asteroidCoords.map(coord => coord.toString()));
   let bestAsteroid, highestCount
-  // console.log(asteroidCoords)
-  // console.log(coordStrings);
 
-  console.log(clearPath([5, 2], [3, 6], asteroidCoords, coordStrings))
   asteroidCoords.forEach(coord => {
     const currCount = asteroid(coord, asteroidCoords, coordStrings);
-    // if (currCount >= 40) console.log(coord);
-    // console.log(currCount);
     if (!highestCount || currCount > highestCount) {
       highestCount = currCount;
       bestAsteroid = coord;
@@ -50,11 +45,8 @@ function asteroid(asteroidCoord, allCoords, coordStrings) {
 }
 
 function clearPath(coord1, coord2, allCoords, coordStrings) {
-  console.log("Coord 1: ", coord1)
-  console.log("Coord 2: ", coord2)
   if (coord1[0] === coord2[0] && coord1[1] === coord2[1]) return false;
   const slope = (coord2[1] - coord1[1]) / (coord2[0] - coord1[0]);
-  // console.log(slope);
 
   if (isNaN(slope)) {
     return !(allCoords.some(coord => coord[0] === 0 && between(coord[1], coord1[1], coord2[1])) ||
@@ -71,23 +63,9 @@ function clearPath(coord1, coord2, allCoords, coordStrings) {
     let yIncrement = slope;
     let xIncrement = 1;
 
-    if (slope < 0) {
-      if (coord1[0] > coord2[0]) {
-        yIncrement = -slope;
-        xIncrement = -1;
-      }
-    } else {
-      if (coord1[0] > coord2[0]) {
-        yIncrement = -slope;
-        xIncrement = -1;
-      }
-    }
-
-    if (slope > -1 && slope < 1) {
-      // console.log(slope);
-      yIncrement *= (1 / slope);
-      xIncrement *= (1 / slope);
-      // console.log(yIncrement, xIncrement)
+    if (coord1[0] > coord2[0]) {
+      yIncrement = -slope;
+      xIncrement = -1;
     }
 
     while (Math.abs(parseFloat(nextY.toFixed(4)) - coord1[1]) < Math.abs(coord2[1] - coord1[1]) &&
@@ -95,9 +73,6 @@ function clearPath(coord1, coord2, allCoords, coordStrings) {
       nextY += yIncrement;
       nextX += xIncrement;
       const nextCoord = [parseFloat(nextX.toFixed(4)), parseFloat(nextY.toFixed(4))].toString();
-      console.log("New coords...");
-      console.log([nextX, nextY])
-      console.log(nextCoord);
 
       if (![coord1Str, coord2Str].includes(nextCoord)) {
         if (coordStrings.has(nextCoord)) return false;
@@ -105,7 +80,6 @@ function clearPath(coord1, coord2, allCoords, coordStrings) {
     }
 
   }
-  console.log("Hit!!")
   return true;
 }
 
